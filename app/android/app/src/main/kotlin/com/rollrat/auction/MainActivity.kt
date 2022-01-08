7ae667b0f9f0dd019ca78da25cbab36f05a11ef1
rollrat <rollrat.cse@gmail.com>
2022-01-08 03:09:29 +0000

    android(app): fix build error on kotlin default code

--- app/android/app/src/main/kotlin/com/rollrat/auction/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/rollrat/auction/MainActivity.kt
@@ -15,7 +15,4 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
