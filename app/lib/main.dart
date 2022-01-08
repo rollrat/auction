c1ef38d50bf855b47abad5b01db49b4106379708
rollrat <rollrat.cse@gmail.com>
2022-01-08 03:02:04 +0000

    android(app): disable google services

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,4 @@
-// This source code is a part of Project Violet.
+// This source code is a part of Auction App.
 // Copyright (C) 2022. rollrat. Licensed under the Apache-2.0 License.
 
 import 'package:flutter/material.dart';
