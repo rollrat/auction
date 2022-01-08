b3bd05f1f94d2d4e0249b27384690ede688c1b1b
rollrat <rollrat.cse@gmail.com>
2022-01-08 02:08:42 +0000

    init project

---
+++ app/lib/main.dart
@@ -0,0 +1,38 @@
+// This source code is a part of Project Violet.
+// Copyright (C) 2022. rollrat. Licensed under the Apache-2.0 License.
+
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      body: Center(child: Text('You have pressed the button $_count times.')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _count++),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(
+    home: MainPage(),
+  ));
+}
---
+++ main.js
@@ -0,0 +1,33 @@
+// https://www.data.go.kr/data/15000851/openapi.do
+
+const serviceKey = require('./service-key');
+var request = require('request');
+
+var url = 'http://openapi.onbid.co.kr/openapi/services/KamcoPblsalThingInquireSvc/getKamcoPbctCltrList';
+var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey; /* Service Key*/
+queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* */
+queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+queryParams += '&' + encodeURIComponent('DPSL_MTD_CD') + '=' + encodeURIComponent('0001'); /* */
+queryParams += '&' + encodeURIComponent('CTGR_HIRK_ID') + '=' + encodeURIComponent('10000'); /* */
+queryParams += '&' + encodeURIComponent('CTGR_HIRK_ID_MID') + '=' + encodeURIComponent('10100'); /* */
+queryParams += '&' + encodeURIComponent('SIDO') + '=' + encodeURIComponent('강원도'); /* */
+queryParams += '&' + encodeURIComponent('SGK') + '=' + encodeURIComponent('인제군'); /* */
+queryParams += '&' + encodeURIComponent('EMD') + '=' + encodeURIComponent('남면'); /* */
+queryParams += '&' + encodeURIComponent('GOODS_PRICE_FROM') + '=' + encodeURIComponent('522740000'); /* */
+queryParams += '&' + encodeURIComponent('GOODS_PRICE_TO') + '=' + encodeURIComponent('617393000'); /* */
+queryParams += '&' + encodeURIComponent('OPEN_PRICE_FROM') + '=' + encodeURIComponent('522740000'); /* */
+queryParams += '&' + encodeURIComponent('OPEN_PRICE_TO') + '=' + encodeURIComponent('617393000'); /* */
+queryParams += '&' + encodeURIComponent('CLTR_NM') + '=' + encodeURIComponent('종이팩'); /* */
+queryParams += '&' + encodeURIComponent('PBCT_BEGN_DTM') + '=' + encodeURIComponent('20171218'); /* */
+queryParams += '&' + encodeURIComponent('PBCT_CLS_DTM') + '=' + encodeURIComponent('20171218'); /* */
+queryParams += '&' + encodeURIComponent('CLTR_MNMT_NO') + '=' + encodeURIComponent('2012-1141-001291'); /* */
+
+request({
+    url: url + queryParams,
+    method: 'GET'
+}, function (error, response, body) {
+
+    console.log('Status', response.statusCode);
+    console.log('Headers', JSON.stringify(response.headers));
+    console.log('Reponse received', body);
+});
