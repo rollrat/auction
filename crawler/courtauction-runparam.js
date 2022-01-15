2c7298ababa9a73c48c3465812874e1b830fc22a
rollrat <rollrat.cse@gmail.com>
2022-01-15 11:19:13 +0000

    log(crawler): show log in courtauction-runparam

--- crawler/courtauction-runparam.js
+++ crawler/courtauction-runparam.js
@@ -15,7 +15,7 @@ function delay(time) {
   const page = await browser.newPage();
 
   await page.goto("https://www.courtauction.go.kr/");
-  const frame = await page.waitForFrame(async (frame) => {
+  var frame = await page.waitForFrame(async (frame) => {
     return frame.name() === "indexFrame";
   });
   await frame.waitForSelector("#main_btn");
@@ -32,58 +32,78 @@ function delay(time) {
   const dataLen = Object.keys(data).length;
 
   for (var i = 0; i < dataLen; i++) {
-    await delay(1000);
-    // await frame.waitForSelector(".Ltbl_list");
-    // await frame.waitForSelector(".Ltbl_list_lvl0");
+    try {
+      // await delay(1000);
+      console.log('wait .Ltbl_list');
+      await frame.waitForSelector(".Ltbl_list");
+      // await frame.waitForSelector(".Ltbl_list_lvl0");
 
-    console.log(i);
-    console.log(data[i].param);
-    // 사건내역
-    // await frame.evaluate(
-    //   (e) => detailCaseSrch(e[0], e[1].trim(), e[2]),
-    //   data[i].param
-    // );
-    // 물건 내역
-    await frame.evaluate(
-      (e) => detailSrch(e[0], e[1].trim(), e[2]),
-      data[i].param
-    );
-
-    await frame.waitForSelector(".Ltbl_dt, #wrong_gd");
-
-    // 현재 선택하신 물건은 공고중인 물건이 아닙니다.
-    if ((await frame.$("#wrong_gd")) != null) {
-      console.log("skipped");
-      continue;
-    }
-
-    async function showOuterHtml(element) {
-      const data = await element.evaluate((e) => e.outerHTML);
-      console.log(data);
-    }
-
-    async function getAttr(element, attr) {
-      return await frame.evaluate(
-        (e, attr) => e.getAttribute(attr),
-        element,
-        attr
+      console.log(`Progress: [${i}/${dataLen}]`);
+      console.log(data[i].param);
+      // 사건내역
+      // await frame.evaluate(
+      //   (e) => detailCaseSrch(e[0], e[1].trim(), e[2]),
+      //   data[i].param
+      // );
+      // 물건 내역
+      console.log('eval detailSrch');
+      await frame.evaluate(
+        (e) => detailSrch(e[0], e[1].trim(), e[2]),
+        data[i].param
       );
-    }
 
-    // 이미지 가져오기
-    var photo_urls = [];
-    for (var k = 0; ; k++) {
-      var imgs = await frame.$$(`div#photo${k} img`);
-      if (imgs.length == 0) break;
-      for (var j = 0; j < imgs.length; j++) {
-        photo_urls.push((await getAttr(imgs[j], "src")).replace(/T_/gi, ""));
+      console.log('wait .Ltbl_dt, #wrong_gd');
+      await frame.waitForSelector(".Ltbl_dt, #wrong_gd");
+
+      // 현재 선택하신 물건은 공고중인 물건이 아닙니다.
+      if ((await frame.$("#wrong_gd")) != null) {
+        console.log("skipped");
+        continue;
       }
+
+      async function showOuterHtml(element) {
+        const data = await element.evaluate((e) => e.outerHTML);
+        console.log(data);
+      }
+
+      async function getAttr(element, attr) {
+        return await frame.evaluate(
+          (e, attr) => e.getAttribute(attr),
+          element,
+          attr
+        );
+      }
+
+      // 이미지 가져오기
+      console.log('get images');
+      var photo_urls = [];
+      for (var k = 0; ; k++) {
+        var imgs = await frame.$$(`div#photo${k} img`);
+        if (imgs.length == 0) break;
+        for (var j = 0; j < imgs.length; j++) {
+          photo_urls.push((await getAttr(imgs[j], "src")).replace(/T_/gi, ""));
+        }
+      }
+
+      console.log(photo_urls);
+
+      // 이전으로
+      console.log('eval porBefSrnActSubmit');
+      await frame.evaluate(() => porBefSrnActSubmit());
+    } catch (e) {
+      console.log(e);
+      try {
+        await page.goto("https://www.courtauction.go.kr/");
+        frame = await page.waitForFrame(async (frame) => {
+          return frame.name() === "indexFrame";
+        });
+        await frame.waitForSelector("#main_btn");
+        await frame.evaluate(() => fastSrch());
+
+      } catch (e) {
+      }
+      i--;
     }
-
-    console.log(photo_urls);
-
-    // 이전으로
-    await frame.evaluate(() => porBefSrnActSubmit());
   }
 
   await browser.close();
