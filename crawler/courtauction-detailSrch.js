19d0f3e3d63ae23d7bc07cde4eff3cdc0ba4ac0c
rollrat <rollrat.cse@gmail.com>
2022-02-19 11:20:18 +0000

    impl(crawler): implements detailSrch parser

--- crawler/courtauction-detailSrch.js
+++ crawler/courtauction-detailSrch.js
@@ -4,6 +4,7 @@
 const request = require("request");
 const Iconv = require("iconv").Iconv;
 const jschardet = require("jschardet");
+const parser = require("node-html-parser");
 
 // var ii = new iconv.Iconv("euc-kr", "utf-8");
 
@@ -14,7 +15,6 @@ function anyToUtf8(str) {
 }
 
 function anyToEUCKR(str) {
-  const { encoding } = jschardet.detect(str);
   const iconv = new Iconv("utf-8", "euc-kr");
   return escape(iconv.convert(str).toString("binary"));
 }
@@ -26,82 +26,143 @@ function encodeForm(form) {
     arrs.push(`${key}=${form[key]}`);
   });
 
-  console.log(arrs.join("&"));
-
   return arrs.join("&");
 }
 
-async function detailSrch(vJiwonNm, vSaNo, vMaemulSer) {
-  request.post(
-    {
-      uri: "https://www.courtauction.go.kr/RetrieveRealEstCarHvyMachineMulDetailInfo.laf",
-      body: encodeForm({
-        jiwonNm: anyToEUCKR(vJiwonNm),
-        saNo: encodeURI(vSaNo.trim()),
-        maemulSer: encodeURI(vMaemulSer),
-        // _NEXT_SRNID: "PNO102015",
-
-        // jiwonNm: %BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8
-        // saNo: 20210130100084
-        // maemulSer: 1
-        // mokmulSer: "",
-        // _NAVI_CMD: "RetrieveMainInfo.laf%5EInitMulSrch.laf",
-        // _NAVI_SRNID: "PNO102000%5EPNO102001",
-        // _SRCH_SRNID: "PNO102001",
-        // _CUR_CMD: "RetrieveRealEstMulDetailList.laf",
-        // _CUR_SRNID: "PNO102002",
-        // _NEXT_CMD: "RetrieveRealEstCarHvyMachineMulDetailInfo.laf",
-        // _NEXT_SRNID: "PNO102015",
-        // _PRE_SRNID: "",
-        // _LOGOUT_CHK: "",
-        // _FORM_YN: "Y",
-        // _C_srnID: "PNO102000",
-        // _C_jiwonNm: "%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8",
-        // _C_bubwLocGubun: 1,
-        // _C_jibhgwanOffMgakPlcGubun: "",
-        // _C_mvmPlaceSidoCd: "",
-        // _C_mvmPlaceSiguCd: "",
-        // _C_roadPlaceSidoCd: "",
-        // _C_roadPlaceSiguCd: "",
-        // _C_daepyoSidoCd: "",
-        // _C_daepyoSiguCd: "",
-        // _C_daepyoDongCd: "",
-        // _C_rd1Cd: "",
-        // _C_rd2Cd: "",
-        // _C_rd3Rd4Cd: "",
-        // _C_roadCode: "",
-        // _C_notifyLoc: 1,
-        // _C_notifyRealRoad: 1,
-        // _C_notifyNewLoc: 1,
-        // _C_mvRealGbncd: 1,
-        // _C_jiwonNm1: "%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8",
-        // _C_jiwonNm2: "%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8",
-        // _C_mDaepyoSidoCd: "",
-        // _C_mvDaepyoSidoCd: "",
-        // _C_mDaepyoSiguCd: "",
-        // _C_mvDaepyoSiguCd: "",
-        // _C_realVowel: "00000_55203",
-        // _C_vowelSel: "00000_55203",
-        // _C_mDaepyoDongCd: "",
-        // _C_mvmPlaceDongCd: "",
-      }),
-      //         "jiwonNm=%EC%84%9C%EC%9A%B8%EC%A4%91%EC%95%99%EC%A7%80%EB%B0%A9%EB%B2%95%EC%9B%90&saNo=2020210130100084&maemulSer=1&mokmulSer=&_NAVI_CMD=RetrieveMainInfo.laf%5EInitMulSrch.laf&_NAVI_SRNID=PNO102000%5EPNO102001&_SRCH_SRNID=PNO102001&_CUR_CMD=RetrieveRealEstMulDetailList.laf&_CUR_SRNID=PNO102002&_NEXT_CMD=RetrieveRealEstCarHvyMachineMulDetailInfo.laf&_NEXT_SRNID=PNO102015&_PRE_SRNID=&_LOGOUT_CHK=&_FORM_YN=Y&_C_srnID=PNO102000&_C_jiwonNm=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&_C_bubwLocGubun=1&_C_jibhgwanOffMgakPlcGubun=&_C_mvmPlaceSidoCd=&_C_mvmPlaceSiguCd=&_C_roadPlaceSidoCd=&_C_roadPlaceSiguCd=&_C_daepyoSidoCd=&_C_daepyoSiguCd=&_C_daepyoDongCd=&_C_rd1Cd=&_C_rd2Cd=&_C_rd3Rd4Cd=&_C_roadCode=&_C_notifyLoc=1&_C_notifyRealRoad=1&_C_notifyNewLoc=1&_C_mvRealGbncd=1&_C_jiwonNm1=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&_C_jiwonNm2=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&_C_mDaepyoSidoCd=&_C_mvDaepyoSidoCd=&_C_mDaepyoSiguCd=&_C_mvDaepyoSiguCd=&_C_realVowel=00000_55203&_C_vowelSel=00000_55203&_C_mDaepyoDongCd=&_C_mvmPlaceDongCd="
-      //   body: "jiwonNm=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&saNo=20210130100084&maemulSer=1&mokmulSer=&_NAVI_CMD=RetrieveMainInfo.laf%5EInitMulSrch.laf&_NAVI_SRNID=PNO102000%5EPNO102001&_SRCH_SRNID=PNO102001&_CUR_CMD=RetrieveRealEstMulDetailList.laf&_CUR_SRNID=PNO102002&_NEXT_CMD=RetrieveRealEstCarHvyMachineMulDetailInfo.laf&_NEXT_SRNID=PNO102015&_PRE_SRNID=&_LOGOUT_CHK=&_FORM_YN=Y&_C_srnID=PNO102000&_C_jiwonNm=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&_C_bubwLocGubun=1&_C_jibhgwanOffMgakPlcGubun=&_C_mvmPlaceSidoCd=&_C_mvmPlaceSiguCd=&_C_roadPlaceSidoCd=&_C_roadPlaceSiguCd=&_C_daepyoSidoCd=&_C_daepyoSiguCd=&_C_daepyoDongCd=&_C_rd1Cd=&_C_rd2Cd=&_C_rd3Rd4Cd=&_C_roadCode=&_C_notifyLoc=1&_C_notifyRealRoad=1&_C_notifyNewLoc=1&_C_mvRealGbncd=1&_C_jiwonNm1=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&_C_jiwonNm2=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8&_C_mDaepyoSidoCd=&_C_mvDaepyoSidoCd=&_C_mDaepyoSiguCd=&_C_mvDaepyoSiguCd=&_C_realVowel=00000_55203&_C_vowelSel=00000_55203&_C_mDaepyoDongCd=&_C_mvmPlaceDongCd=",
-      headers: {
-        "user-agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-        accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "content-type": "application/x-www-form-urlencoded",
-        cookie:
-          "WMONID=G1elsQlYpTn; JSESSIONID=ephPJY9QpvO5scLbaB7WHN7YqOVh5aBCsz4PoqSw4zIgbgCG75xdOxnaQzx2VZN9.amV1c19kb21haW4vYWlzMg==; realJiwonNm=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8; daepyoSidoCd=; daepyoSiguCd=; mvmPlaceSidoCd=; mvmPlaceSiguCd=; rd1Cd=; rd2Cd=; realVowel=35207_45207; roadPlaceSidoCd=; roadPlaceSiguCd=; vowelSel=35207_45207; toMul=%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8%2C20210130100084%2C1%2C20220302%2CB%5E%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8%2C20200130005373%2C4%2C20220302%2CB%5E%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8%2C20200130110930%2C1%2C20220302%2CB%5E%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8%2C20200130108517%2C1%2C20220302%2CB%5E%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8%2C20200130004899%2C1%2C20220302%2CB%5E%BC%AD%BF%EF%C1%DF%BE%D3%C1%F6%B9%E6%B9%FD%BF%F8%2C20200130004462%2C1%2C20220302%2CB%5E; locIdx=202101301000841",
-      },
-      encoding: null,
-    },
-    function (err, httpResponse, body) {
-      console.log(anyToUtf8(body));
-    }
-  );
+function doPost(options) {
+  return new Promise(function (resolve, reject) {
+    request.post(options, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(anyToUtf8(body));
+      } else {
+        reject(error);
+      }
+    });
+  });
 }
 
-detailSrch("서울중앙지방법원", " 20210130100084", "1");
+async function detailSrch(vJiwonNm, vSaNo, vMaemulSer) {
+  return await doPost({
+    uri: "https://www.courtauction.go.kr/RetrieveRealEstCarHvyMachineMulDetailInfo.laf",
+    body: encodeForm({
+      jiwonNm: anyToEUCKR(vJiwonNm),
+      saNo: encodeURI(vSaNo.trim()),
+      maemulSer: encodeURI(vMaemulSer),
+    }),
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    encoding: null,
+  });
+}
+
+function printChildNodeText(e) {
+  var texts = [];
+  for (const child of e.childNodes) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+    // if (child.nodeType === Node.TEXT_NODE) {
+    if (child.nodeType === 3) {
+      var text = child.textContent.trim();
+      if (text != "") texts.push(text);
+    } else {
+      texts.push(...printChildNodeText(child));
+    }
+  }
+  return texts;
+}
+
+function parse_detailSrchHtml(html) {
+  const root = parser.parse(html);
+
+  var objInfo = {};
+
+  //
+  //    1. images
+  //
+  var photo_urls = [];
+  for (var k = 0; ; k++) {
+    var imgs = root.querySelectorAll(`div#photo${k} img`);
+    if (imgs.length == 0) break;
+    for (var j = 0; j < imgs.length; j++) {
+      photo_urls.push(imgs[j].attrs["src"]);
+    }
+  }
+
+  objInfo["photos"] = photo_urls;
+
+  //
+  //    2. 물건기본정보 표
+  //
+  {
+    var tables = root.querySelectorAll(`table.Ltbl_dt`);
+    // [물건기본정보 표, 물건기본정보 표2, 사진, 감정평가요항표]
+    {
+      //
+      //  2-1. 물건기본정보 표
+      //
+      {
+        var trs = tables[0].querySelectorAll("tr");
+        var t1 = printChildNodeText(trs[0]); // 사건번호, 물건번호, 물건종류
+        var t2 = printChildNodeText(trs[1]); // 감정평가액, 최저매각가격, 입찰방법
+        var t3 = printChildNodeText(trs[2]); // 매각기일
+        var t4 = printChildNodeText(trs[3]); // 물건비고
+        var t5 = printChildNodeText(trs[4]); // 목록1 소재지
+        var t6 = printChildNodeText(trs[5]); // 담당
+
+        objInfo["binfo1"] = [t1, t2, t3, t4, t5, t6];
+      }
+
+      //
+      //  2-2. 물건기본정보 표2
+      //
+      {
+        var trs = tables[1].querySelectorAll("tr");
+        var t1 = printChildNodeText(trs[0]); // 사건접수, 경매개시일
+        var t2 = printChildNodeText(trs[1]); // 배당요구종기, 청구금액
+
+        objInfo["binfo2"] = [t1, t2];
+      }
+
+      //
+      //  2-3. 감정평가요항표
+      //
+      {
+        var trs = tables[1].querySelectorAll("tr");
+        var eve = [];
+
+        for (var trsi = 0; trsi < trs.length; trsi++) {
+          eve.push(printChildNodeText(trs[trsi]));
+        }
+
+        objInfo["eve"] = [eve];
+      }
+    }
+  }
+
+  //
+  //  3. 기일내역
+  //
+  {
+    var table = root.querySelectorAll("table.Ltbl_list")[0];
+    // 기일, 기일종류, 기일장소, 최저매각가격, 기일결과
+    var trs = table.querySelectorAll("tr");
+    var dal = [];
+
+    for (var trsi = 0; trsi < trs.length; trsi++) {
+      dal.push(printChildNodeText(trs[trsi]));
+    }
+
+    objInfo["dal"] = dal;
+  }
+
+  return objInfo;
+}
+
+detailSrch("서울중앙지방법원", " 20210130100084", "1").then((e) => {
+  //   console.log(e);
+  console.log(parse_detailSrchHtml(e));
+});
