b9b7c0fa157f5aac5bf63c205069a5a26089a7a0
rollrat <rollrat.cse@gmail.com>
2022-03-12 16:30:07 +0900

    app(package): update android and windows code

--- app/android/app/src/main/kotlin/com/rollrat/auction/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/rollrat/auction/MainActivity.kt
@@ -1,17 +1,6 @@
 package com.rollat.auction
 
-import android.app.Activity
-import android.net.VpnService
-import android.os.Build
-import android.os.Bundle
-import android.view.KeyEvent
-import android.view.View
-import android.view.WindowManager
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.EventChannel
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
