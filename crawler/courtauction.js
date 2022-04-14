5c6a101d6439427c35a359b6bfa5b72c7433d67f
rollrat <rollrat.cse@gmail.com>
2022-04-14 15:51:21 +0900

    crawler(docker): update dockerfile and run puppeteer with no-sandbox

--- crawler/courtauction.js
+++ crawler/courtauction.js
@@ -11,7 +11,9 @@ function delay(time) {
 }
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
 
   await page.goto("https://www.courtauction.go.kr/");
