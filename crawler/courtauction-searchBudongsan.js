e23e4c14ac815a161a4916215f50b72674bdb140
rollrat <rollrat.cse@gmail.com>
2022-04-27 22:27:42 +0900

    crawler: transform for github actions

--- crawler/courtauction-searchBudongsan.js
+++ crawler/courtauction-searchBudongsan.js
@@ -4,20 +4,55 @@
 const searchBudongsan = require('./api/courtauction/searchBudongsan');
 const fs = require("fs");
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 (async () => {
   var results = [];
+  var complete = 0;
+  var prevComplete = 0;
+  var occurerror = false;
 
-  for (var i = 0; i < 10000; i++) {
-    try {
-      var v = await searchBudongsan.searchBudongsan('전체', 1 + i * 20, null);
-      console.log(searchBudongsan.parse_searchBudongsan(v[0]));
-      results.push(searchBudongsan.parse_searchBudongsan(v[0]));
-    } catch (e) {
-      console.log(e);
-      break;
+  for (var i = 0; i < 1000; i++) {
+    results.push();
+  }
+
+  for (var i = 0; i < 1000; i++) {
+    searchBudongsan.searchBudongsan('전체', 1 + i * 20, i).then((e) => {
+      try {
+        results[e[1]] = searchBudongsan.parse_searchBudongsan(e[0]);
+        console.log (results[e[1]]);
+        complete++;
+      } catch (e) {
+        occurerror = true;
+      }
+    });
+    if (i % 5 == 4) {
+      while (!occurerror && complete == i + 1) {
+        await sleep(100);
+      }
+      if (occurerror) break;
     }
-    console.log(i);
-  } 
+  }
+
+
+  // for (var i = 0; i < 1000; i++) {
+
+
+
+  //   try {
+  //     var v = await searchBudongsan.searchBudongsan('전체', 1 + i * 20, null);
+  //     console.log(searchBudongsan.parse_searchBudongsan(v[0]));
+  //     results.push(searchBudongsan.parse_searchBudongsan(v[0]));
+  //   } catch (e) {
+  //     console.log(e);
+  //     break;
+  //   }
+  //   console.log(i);
+  // } 
 
   fs.writeSync(fs.openSync("result.json", "w"), JSON.stringify(results));
 })();
